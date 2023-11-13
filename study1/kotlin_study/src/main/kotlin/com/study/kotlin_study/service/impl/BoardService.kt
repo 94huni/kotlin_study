c35c92dae3ee95603361a5934522c35c6d5cd30b
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 15:12:40 +0900

    BoardService interface createBoard 생성

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/impl/BoardService.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/impl/BoardService.kt
@@ -1,8 +1,12 @@
 package com.study.kotlin_study.service.impl
 
+import com.study.kotlin_study.dto.request.BoardCreateRequest
 import com.study.kotlin_study.dto.response.BoardDTO
 import com.study.kotlin_study.entity.Board
+import com.study.kotlin_study.entity.Member
 
 interface BoardService {
     fun getBoard(boardId: Long): BoardDTO
+
+    fun createBoard(request: BoardCreateRequest, member: Member): BoardDTO
 }
