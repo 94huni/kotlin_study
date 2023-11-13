af3d476686ad87a8154879eb63157497ab472d25
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 15:11:25 +0900

    loggerFactory 추가

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/BoardServiceImpl.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/BoardServiceImpl.kt
@@ -1,19 +1,26 @@
 package com.study.kotlin_study.service
 
+import com.study.kotlin_study.dto.request.BoardCreateRequest
 import com.study.kotlin_study.dto.response.BoardDTO
 import com.study.kotlin_study.entity.Board
+import com.study.kotlin_study.entity.Member
 import com.study.kotlin_study.repository.BoardRepository
 import com.study.kotlin_study.repository.MemberRepository
 import com.study.kotlin_study.service.impl.BoardService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class BoardServiceImpl (
     private val boardRepository: BoardRepository,
     private val memberRepository: MemberRepository
 ) : BoardService {
+    val logger: Logger = LoggerFactory.getLogger(BoardService::class.java)
 
     private fun toDTO(board: Board, writer: String): BoardDTO {
+
         return BoardDTO(
             boardId = board.id,
             title = board.title,
@@ -25,9 +32,27 @@ class BoardServiceImpl (
     }
     override fun getBoard(boardId: Long): BoardDTO {
         val board = boardRepository.findById(boardId).get()
-        val member = memberRepository.findById(board.memberId)
-        val writer: String = member.get().nickname
+        val member: Member = memberRepository.findById(board.memberId)
+        val writer: String = member.nickname
+
+        logger.info("writer : {} boardId : {} ", writer, board.id)
 
         return toDTO(board, writer)
+    }
+
+    override fun createBoard(request: BoardCreateRequest, member: Member): BoardDTO {
+
+        logger.info("title : {} writer: {}", request.title, member.nickname)
+
+        val board = Board(
+            title = request.title,
+            content = request.content,
+            createTime = LocalDateTime.now(),
+            memberId = member.id
+        )
+
+        val result : Board = boardRepository.save(board)
+
+        return toDTO(result, member.nickname)
     }
 }
