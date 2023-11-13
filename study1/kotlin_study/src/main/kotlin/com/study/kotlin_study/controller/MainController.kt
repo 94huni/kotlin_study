b7baacfabcac8032d38c89608a1ccb5690abed69
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 10:55:04 +0900

    기본 controller 생성

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/MainController.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/MainController.kt
@@ -1,4 +1,13 @@
 package com.study.kotlin_study.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class MainController {
+
+    @GetMapping("")
+    fun main():String {
+        return "Hello"
+    }
 }
