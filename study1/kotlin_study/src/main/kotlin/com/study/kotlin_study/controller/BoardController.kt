b9c62fb921bec08279c8f0eea53c4999fe11c3ed
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 16:04:21 +0900

    Board modify controller

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/BoardController.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/BoardController.kt
@@ -2,12 +2,14 @@ package com.study.kotlin_study.controller
 
 import com.study.kotlin_study.dto.request.BoardCreateRequest
 import com.study.kotlin_study.dto.response.BoardDTO
+import com.study.kotlin_study.entity.Member
 import com.study.kotlin_study.service.MemberService
 import com.study.kotlin_study.service.impl.BoardService
 import jakarta.servlet.http.HttpSession
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -29,5 +31,13 @@ class BoardController (
         val member = memberService.findMember(writer)
 
         return boardService.createBoard(request, member)
+    }
+
+    @PutMapping("/{boardId}")
+    fun modifyBoard(@PathVariable boardId: Long, @RequestBody request: BoardCreateRequest, session: HttpSession): String {
+        val member: Member = memberService.findMember(session.getAttribute("Member").toString())
+        boardService.modifyBoard(request, boardId, member)
+
+        return "$boardId 수정 완료"
     }
 }
