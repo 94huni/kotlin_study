67ff62026abbd35393bd49164ed34da1413b4439
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 12:02:44 +0900

    회원가입 로직

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/MemberService.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/MemberService.kt
@@ -1,9 +1,12 @@
 package com.study.kotlin_study.service
 
 import com.study.kotlin_study.dto.request.LoginRequest
+import com.study.kotlin_study.dto.request.SignUpRequest
 import com.study.kotlin_study.entity.Member
 import com.study.kotlin_study.repository.MemberRepository
+import org.springframework.stereotype.Service
 
+@Service
 class MemberService (
     private val memberRepository: MemberRepository
 ) {
@@ -15,5 +18,23 @@ class MemberService (
         }
 
         return member.email
+    }
+
+    fun signUp(signUpRequest: SignUpRequest): String {
+        if(signUpRequest.password != signUpRequest.validPassword) throw RuntimeException("비밀번호 불일치")
+
+        if(memberRepository.existsByEmail(signUpRequest.email)) throw RuntimeException("이미 사용중인 이메일")
+
+        val member = Member(
+            email = signUpRequest.email,
+            name = signUpRequest.name,
+            nickname = signUpRequest.nickname,
+            password = signUpRequest.password,
+            phone = signUpRequest.phone
+        )
+
+        memberRepository.save(member)
+
+        return "회원 가입 성공"
     }
 }
