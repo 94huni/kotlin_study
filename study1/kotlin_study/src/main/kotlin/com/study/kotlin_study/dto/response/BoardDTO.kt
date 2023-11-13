05d77058dcf387c80efe0ec61dc5c19dd0d2585e
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 15:10:05 +0900

    Response DTO class null 허용

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/dto/response/BoardDTO.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/dto/response/BoardDTO.kt
@@ -3,10 +3,10 @@ package com.study.kotlin_study.dto.response
 import java.time.LocalDateTime
 
 data class BoardDTO(
-    val boardId: Long,
-    val title: String,
-    val content: String,
+    val boardId: Long?,
+    val title: String?,
+    val content: String?,
     val writer: String,
     val createTime: LocalDateTime,
-    val updateTime: LocalDateTime
+    val updateTime: LocalDateTime?
 )
