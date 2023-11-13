4a117c469f93a52c11d7b5f642d768738c4d347a
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 12:02:22 +0900

    중복아이디 검증

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/repository/MemberRepository.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/repository/MemberRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository
 
 interface MemberRepository : CrudRepository<Member, Long>{
     fun findByEmail(email: String): Member
+
+    fun existsByEmail(email: String?): Boolean
 }
