0f17dcba9934473c8fd0b7ea0e1ccb67499b0c0f
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 12:00:48 +0900

    null 변경

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/dto/request/SignUpRequest.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/dto/request/SignUpRequest.kt
@@ -1,10 +1,10 @@
 package com.study.kotlin_study.dto.request
 
 data class SignUpRequest(
-    val name: String?,
-    val nickname: String?,
-    val email: String?,
-    val phone: String?,
-    val password: String?,
-    val validPassword: String?
+    val name: String,
+    val nickname: String,
+    val email: String,
+    val phone: String,
+    val password: String,
+    val validPassword: String
 )
