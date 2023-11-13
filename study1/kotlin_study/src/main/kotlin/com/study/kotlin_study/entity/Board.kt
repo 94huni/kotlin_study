b24381f911cc4cbb25f3ac3d4a2e039647077a94
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 13:12:08 +0900

    Board Entity

---
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/entity/Board.kt
@@ -0,0 +1,27 @@
+package com.study.kotlin_study.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
+@Entity
+class Board (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(length = 50)
+    val title: String,
+
+    @Column
+    val content: String,
+
+    @Column(name = "m_id")
+    val memberId: Long,
+
+    val createTime: LocalDateTime,
+
+    val updateTime: LocalDateTime
+)
