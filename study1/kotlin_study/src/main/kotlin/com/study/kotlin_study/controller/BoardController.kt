bb5c73f26af481a22e5826bd33bcfd35b870b7d9
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 18:11:22 +0900

    Board delete controller

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/BoardController.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/BoardController.kt
@@ -2,17 +2,12 @@ package com.study.kotlin_study.controller
 
 import com.study.kotlin_study.dto.request.BoardCreateRequest
 import com.study.kotlin_study.dto.response.BoardDTO
+import com.study.kotlin_study.entity.Board
 import com.study.kotlin_study.entity.Member
 import com.study.kotlin_study.service.MemberService
 import com.study.kotlin_study.service.impl.BoardService
 import jakarta.servlet.http.HttpSession
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/board")
@@ -39,5 +34,14 @@ class BoardController (
         boardService.modifyBoard(request, boardId, member)
 
         return "$boardId 수정 완료"
+    }
+
+    @DeleteMapping("/{boardId}")
+    fun delete(@PathVariable boardId: Long, session: HttpSession):String {
+        val member :Member = memberService.findMember(session.getAttribute("Member").toString())
+        val board :Board = boardService.getBoardEntity(boardId)
+        boardService.deleteBoard(board, member)
+
+        return "${board.title} 글이 삭제 되었습니다."
     }
 }
