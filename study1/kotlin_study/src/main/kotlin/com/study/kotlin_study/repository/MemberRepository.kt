9c24fc351f93c73a343e59b8bc7d159474d030e6
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 15:11:03 +0900

    findById null 값 일때 처리

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/repository/MemberRepository.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/repository/MemberRepository.kt
@@ -4,6 +4,8 @@ import com.study.kotlin_study.entity.Member
 import org.springframework.data.repository.CrudRepository
 
 interface MemberRepository : CrudRepository<Member, Long>{
+
+    fun findById(id: Long?): Member
     fun findByEmail(email: String): Member
 
     fun existsByEmail(email: String?): Boolean
