77a248f562ade0da118d8c3aef2a8614bd30b217
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 12:00:01 +0900

    url 변경 + 회원가입

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/MemberController.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/MemberController.kt
@@ -1,10 +1,15 @@
 package com.study.kotlin_study.controller
 
 import com.study.kotlin_study.dto.request.LoginRequest
+import com.study.kotlin_study.dto.request.SignUpRequest
 import com.study.kotlin_study.service.MemberService
 import jakarta.servlet.http.HttpSession
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
+@RequestMapping("/api/v1/auth")
 class MemberEmail(
     private val memberService: MemberService
 ) {
@@ -15,5 +20,10 @@ class MemberEmail(
         session.setAttribute("Member", result)
 
         return result
+    }
+
+    @PostMapping("/sign-up")
+    fun signUp(signUpRequest: SignUpRequest): String {
+        return memberService.signUp(signUpRequest)
     }
 }
