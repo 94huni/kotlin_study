4392984979ba57a6a8cfbc7eafaec0289ebf900c
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-13 15:09:31 +0900

    Board Create Conroller

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/BoardController.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/BoardController.kt
@@ -1,18 +1,33 @@
 package com.study.kotlin_study.controller
 
+import com.study.kotlin_study.dto.request.BoardCreateRequest
+import com.study.kotlin_study.dto.response.BoardDTO
+import com.study.kotlin_study.service.MemberService
 import com.study.kotlin_study.service.impl.BoardService
+import jakarta.servlet.http.HttpSession
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/board")
 class BoardController (
-    private val boardService: BoardService
+    private val boardService: BoardService,
+    private val memberService: MemberService
 ){
     @GetMapping("/{boardId}")
-    fun getBoard(@PathVariable boardId: Long) {
-        return
+    fun getBoard(@PathVariable boardId: Long): BoardDTO {
+        return boardService.getBoard(boardId)
+    }
+
+    @PostMapping
+    fun createBoard(@RequestBody request: BoardCreateRequest, session: HttpSession): BoardDTO {
+        val writer : String = session.getAttribute("Member").toString()
+        val member = memberService.findMember(writer)
+
+        return boardService.createBoard(request, member)
     }
 }
