bca1f8a45ac3ff7a5cc6651ee24307eed04146c2
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-14 09:55:58 +0900

    comment service interface

---
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/impl/CommentService.kt
@@ -0,0 +1,16 @@
+package com.study.kotlin_study.service.impl
+
+import com.study.kotlin_study.dto.request.CommentRequest
+import com.study.kotlin_study.dto.response.CommentDTO
+import com.study.kotlin_study.entity.Comment
+import com.study.kotlin_study.entity.Member
+
+interface CommentService {
+    fun createComment(commentRequest: CommentRequest, member: Member): CommentDTO
+
+    fun getComment(boardId: Long): List<CommentDTO>
+
+    fun modifyComment(commentId: Long, commentRequest: CommentRequest, member: Member)
+
+    fun deleteComment(commentId: Long, member: Member)
+}
