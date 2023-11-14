a64f5776b2abf06cc563e2bf494cf0e7850d5f46
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-14 09:57:25 +0900

    생성자

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/CommentServiceImpl.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/CommentServiceImpl.kt
@@ -3,11 +3,13 @@ package com.study.kotlin_study.service
 import com.study.kotlin_study.dto.request.CommentRequest
 import com.study.kotlin_study.dto.response.CommentDTO
 import com.study.kotlin_study.entity.Member
+import com.study.kotlin_study.repository.CommentRepository
 import com.study.kotlin_study.service.impl.CommentService
 import org.springframework.stereotype.Service
 
 @Service
 class CommentServiceImpl(
+    private val commentRepository: CommentRepository
 
 ) : CommentService {
     override fun createComment(commentRequest: CommentRequest, member: Member): CommentDTO {
