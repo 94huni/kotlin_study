b4c8d280f6818ced7077e63e1e62c008d36d921c
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-14 10:40:35 +0900

    dto 변환 + comment create 추가

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/CommentServiceImpl.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/CommentServiceImpl.kt
@@ -2,18 +2,43 @@ package com.study.kotlin_study.service
 
 import com.study.kotlin_study.dto.request.CommentRequest
 import com.study.kotlin_study.dto.response.CommentDTO
+import com.study.kotlin_study.entity.Board
+import com.study.kotlin_study.entity.Comment
 import com.study.kotlin_study.entity.Member
 import com.study.kotlin_study.repository.CommentRepository
 import com.study.kotlin_study.service.impl.CommentService
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class CommentServiceImpl(
     private val commentRepository: CommentRepository
 
 ) : CommentService {
-    override fun createComment(commentRequest: CommentRequest, member: Member): CommentDTO {
-        TODO("Not yet implemented")
+
+    private fun toDTO(com: Comment, member: Member, board: Board): CommentDTO {
+
+        return CommentDTO(
+            commentId = com.id,
+            writer = member.nickname,
+            boardId = board.id,
+            comment = com.comment,
+            createTime = com.createTime,
+            updateTime = com.updateTime
+        )
+    }
+    override fun createComment(commentRequest: CommentRequest, member: Member, board: Board): CommentDTO {
+        val comment = Comment(
+            comment = commentRequest.content,
+            memberId = member.id,
+            boardId = board.id,
+            createTime = LocalDateTime.now(),
+            updateTime = null
+        )
+
+        val result = commentRepository.save(comment)
+
+        return toDTO(result, member, board)
     }
 
     override fun getComment(boardId: Long): List<CommentDTO> {
