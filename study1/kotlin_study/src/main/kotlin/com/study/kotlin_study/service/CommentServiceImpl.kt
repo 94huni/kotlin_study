832d60ea824112008e5604ca2c260aca2f25a360
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-14 09:55:28 +0900

    comment service interface

---
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/CommentServiceImpl.kt
@@ -0,0 +1,29 @@
+package com.study.kotlin_study.service
+
+import com.study.kotlin_study.dto.request.CommentRequest
+import com.study.kotlin_study.dto.response.CommentDTO
+import com.study.kotlin_study.entity.Member
+import com.study.kotlin_study.service.impl.CommentService
+import org.springframework.stereotype.Service
+
+@Service
+class CommentServiceImpl(
+
+) : CommentService {
+    override fun createComment(commentRequest: CommentRequest, member: Member): CommentDTO {
+        TODO("Not yet implemented")
+    }
+
+    override fun getComment(boardId: Long): List<CommentDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override fun modifyComment(commentId: Long, commentRequest: CommentRequest, member: Member) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteComment(commentId: Long, member: Member) {
+        TODO("Not yet implemented")
+    }
+
+}
