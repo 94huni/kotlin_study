b7d25bf551d927a394606d3b03417ca402da12ce
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-14 09:40:44 +0900

    comment Request DTO

---
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/dto/response/CommentDTO.kt
@@ -0,0 +1,11 @@
+package com.study.kotlin_study.dto.response
+
+import java.time.LocalDateTime
+
+data class CommentDTO(
+    val commentId: Long?,
+    val comment: String?,
+    val writer: String?,
+    val createTime: LocalDateTime?,
+    val updateTime: LocalDateTime?
+)
