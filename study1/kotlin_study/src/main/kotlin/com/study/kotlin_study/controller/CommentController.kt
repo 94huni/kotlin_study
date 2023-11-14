b71280f007b30b44bb8f816aa73e1d6fdff0aba2
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-14 10:41:43 +0900

    Comment Controller 추가 , 댓글 등록 추가

---
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/controller/CommentController.kt
@@ -0,0 +1,35 @@
+package com.study.kotlin_study.controller
+
+import com.study.kotlin_study.dto.request.CommentRequest
+import com.study.kotlin_study.dto.response.CommentDTO
+import com.study.kotlin_study.service.impl.BoardService
+import com.study.kotlin_study.service.impl.CommentService
+import com.study.kotlin_study.service.impl.MemberService
+import jakarta.servlet.http.HttpSession
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/comment")
+class CommentController (
+    private val commentService: CommentService,
+    private val memberService: MemberService,
+    private val boardService: BoardService
+    ){
+
+    @PostMapping("{boardId}")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    fun createComment(session: HttpSession,
+                      @PathVariable boardId: Long,
+                      @RequestBody commentRequest: CommentRequest): CommentDTO {
+        val member = memberService.findMember(session.getAttribute("Member").toString())
+        val board = boardService.getBoardEntity(boardId)
+
+        return commentService.createComment(commentRequest, member, board)
+    }
+}
