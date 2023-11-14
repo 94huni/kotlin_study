1589a5b42c160a4e50eb3ce3dde4d561bb2d7b74
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-14 09:54:54 +0900

    repository findAll desc

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/repository/CommentRepository.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/repository/CommentRepository.kt
@@ -1,7 +1,10 @@
 package com.study.kotlin_study.repository
 
 import com.study.kotlin_study.entity.Comment
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
 
 interface CommentRepository : CrudRepository<Comment, Long> {
+    fun findAllByOrderByIdDesc(pageable: Pageable) : Page<Comment>
 }
