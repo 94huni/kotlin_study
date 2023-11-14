2ade32766f8fc1586e00242e92406a9243d45774
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-14 10:41:09 +0900

    createComment 매개변수 변경

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/impl/CommentService.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/impl/CommentService.kt
@@ -2,11 +2,12 @@ package com.study.kotlin_study.service.impl
 
 import com.study.kotlin_study.dto.request.CommentRequest
 import com.study.kotlin_study.dto.response.CommentDTO
+import com.study.kotlin_study.entity.Board
 import com.study.kotlin_study.entity.Comment
 import com.study.kotlin_study.entity.Member
 
 interface CommentService {
-    fun createComment(commentRequest: CommentRequest, member: Member): CommentDTO
+    fun createComment(commentRequest: CommentRequest, member: Member, board: Board): CommentDTO
 
     fun getComment(boardId: Long): List<CommentDTO>
 
