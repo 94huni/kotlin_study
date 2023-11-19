aed141932663cda8c6c947887be5112bc8550256
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-19 18:00:14 +0900

    getBoardPage 인터페이스 추가

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/impl/BoardService.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/service/impl/BoardService.kt
@@ -4,11 +4,14 @@ import com.study.kotlin_study.dto.request.BoardCreateRequest
 import com.study.kotlin_study.dto.response.BoardDTO
 import com.study.kotlin_study.entity.Board
 import com.study.kotlin_study.entity.Member
+import org.springframework.data.domain.Page
 
 interface BoardService {
     fun getBoard(boardId: Long): BoardDTO
 
     fun getBoardEntity(boardId: Long): Board
+
+    fun getBoardPage(page:Int, keyword:String?): Page<BoardDTO>
 
     fun createBoard(request: BoardCreateRequest, member: Member): BoardDTO
 
