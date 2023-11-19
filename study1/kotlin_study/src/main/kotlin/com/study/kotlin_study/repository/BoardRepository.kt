b86628f3c9719b31d2d533b3b0303420457a342c
훈NOTE\94_hu <94_hun_i@naver.com>
2023-11-19 17:59:52 +0900

    board repository 검색기능 추가

--- study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/repository/BoardRepository.kt
+++ study1/kotlin_study/src/main/kotlin/com/study/kotlin_study/repository/BoardRepository.kt
@@ -1,8 +1,12 @@
 package com.study.kotlin_study.repository
 
 import com.study.kotlin_study.entity.Board
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
 
 interface BoardRepository : CrudRepository<Board, Long> {
+    fun findAll(pageable: Pageable): Page<Board>
 
+    fun findByTitleOrderByIdDesc(keyword:String, pageable: Pageable) : Page<Board>
 }
